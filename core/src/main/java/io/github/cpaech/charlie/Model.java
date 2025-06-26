2b81fb627d7c83984919840b4368d75ac55bd61c
Knuspriger <da.holz2007@gmail.com>
2025-06-26 08:42:42 +0200

    Update Javadoc Model (noch nicht fertig).java

--- core/src/main/java/io/github/cpaech/charlie/Model.java
+++ core/src/main/java/io/github/cpaech/charlie/Model.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.math.Vector2;
 
 
 public class Model {
-    public Rectangle paddleA = new Rectangle();
-    public Rectangle paddleB = new Rectangle();
-    public Rectangle ball = new Rectangle();
-    public Vector2 ballVelocity = new Vector2();
-    public int scoreA = 0;
-    public int scoreB = 0;
+    public Rectangle paddleA = new Rectangle(); // Linkes Paddle wird als Rechteck implementiert
+    public Rectangle paddleB = new Rectangle(); // Rechtes Paddle wird als Rechteck implementiert
+    public Rectangle ball = new Rectangle(); // Ball wird als Rechteck implementiert
+    public Vector2 ballVelocity = new Vector2(); //
+    public int scoreA = 0; // Attribut mit dem die Anzahl der Punkte von Spieler A gezählt werden
+    public int scoreB = 0; // Attribut mit dem die Anzahl der Punkte von Spieler B gezählt werden
     public int paddleSpeed = 300; // Geschwindigkeit der Paddles in Pixel pro Sekunde
 }
