007f663387b499a572e1e32980eff33b9524a5a9
Knuspriger <da.holz2007@gmail.com>
2025-07-09 10:31:39 +0200

    javadoc Model.java
    fixes #36

--- core/src/main/java/io/github/cpaech/charlie/Model.java
+++ core/src/main/java/io/github/cpaech/charlie/Model.java
@@ -5,16 +5,52 @@ import com.badlogic.gdx.math.Vector2;
 
 
 public class Model {
+    /**
+    * Is set in the lwjgl3 launcher at line 30; "setWindowedMode( 800, 600);". Dynamic resizing is possible but unnecessary for this game.
+    */
     public int screenWidth = 800;
-    public int screenHeight = 600; // are set in the lwjgl3 launcher at line 30; "setWindowedMode( 800, 600);". Dynamic resizing is possible but unnecary for this game.
-    public Rectangle paddleA = new Rectangle(); // implements paddle A as a rectangle
-    public Rectangle paddleB = new Rectangle(); // implements paddle B as a rectangle
-    public int paddleHeight = 100; // sets the paddle height of both paddles
-    public int paddleWidth = 20; // sets the paddle width of both paddles
-    public Rectangle ball = new Rectangle(); // implements the ball as a rectangle
+    /**
+    * Is set in the lwjgl3 launcher at line 30; "setWindowedMode( 800, 600);". Dynamic resizing is possible but unnecessary for this game.
+    */
+    public int screenHeight = 600;
+    /**
+    * Implements paddle A as a rectangle.
+    */
+    public Rectangle paddleA = new Rectangle();
+    /**
+    * Implements paddle B as a rectangle.
+    */
+    public Rectangle paddleB = new Rectangle();
+    /**
+    * Sets the paddle height of both paddles.
+    */
+    public int paddleHeight = 100; 
+    /**
+    * Sets the paddle width of both paddles.
+    */
+    public int paddleWidth = 20;
+    /**
+    * Implements the ball as a rectangle.
+    */
+    public Rectangle ball = new Rectangle();
+    /**
+    * Implements paddle B as a rectangle.
+    */
     public Vector2 ballVelocity = new Vector2();
-    public int scoreA = 0; // implements a score for player A and sets it to 0
-    public int scoreB = 0; // implements a score for player A and sets it to 0
-    public int paddleSpeed = 300; // speed of the paddles in pixel per second
-    public float[] backgroundColor = {0.15f, 0.15f, 0.2f, 1f}; // sets the color of the background
+    /**
+    * Implements a score for player A and sets it to 0.
+    */
+    public int scoreA = 0;
+    /**
+    * Implements a score for player B and sets it to 0.
+    */
+    public int scoreB = 0;
+    /**
+    * Speed of the paddles in pixel per second.
+    */
+    public int paddleSpeed = 300;
+    /**
+    * Sets the color of the background.
+    */
+    public float[] backgroundColor = {0.15f, 0.15f, 0.2f, 1f};
 }
